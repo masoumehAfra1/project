4821b50cf0fb2a42df3f54a85b036a4b955eebe1
afra <afra1.mmd@gmail.com>
2017-09-09 17:47:15 +0430

    five_step

---
+++ TestForGit/app/src/main/java/com/example/dorsa/testforgit/ApiService.java
@@ -0,0 +1,12 @@
+package com.example.dorsa.testforgit;
+
+import android.content.Context;
+
+/**
+ * Created by dorsa on 9/9/2017.
+ */
+
+public class ApiService {
+
+    private Context context;
+}
