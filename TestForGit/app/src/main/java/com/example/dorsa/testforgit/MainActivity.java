299954922a861afd7309ee6650fe14e957bfd436
afra <afra1.mmd@gmail.com>
2017-09-09 17:42:13 +0430

    four_step

--- TestForGit/app/src/main/java/com/example/dorsa/testforgit/MainActivity.java
+++ TestForGit/app/src/main/java/com/example/dorsa/testforgit/MainActivity.java
@@ -2,14 +2,17 @@ package com.example.dorsa.testforgit;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
-
+private ImageView img;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        img= (ImageView) findViewById(R.id.img_ic_launcher);
     }
 }
